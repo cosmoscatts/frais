f6825e92d003126c1f9ff0f4d535576451b56bef
faitsse <2138889191@qq.com>
2022-06-22 15:39:08 +0800

    chore: main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
+
+function init(app: any) {
+  Object.values(import.meta.globEager('./modules/*.ts'))
+    .map(i => i.default)
+    .forEach(i => i.install?.(app))
+  app.mount('#app')
+}
+
+const app = createApp(App)
+init(app)
