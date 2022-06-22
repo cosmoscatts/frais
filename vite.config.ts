6a20e5514cea43f167f0062fe66ff02e147af777
faitsse <2138889191@qq.com>
2022-06-22 15:17:48 +0800

    chore: update vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,95 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Markdown from 'vite-plugin-md'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers'
+import Unocss from 'unocss/vite'
+import { presetAttributify, presetIcons, presetUno, presetWind } from 'unocss'
+import Prism from 'markdown-it-prism'
+import LinkAttributes from 'markdown-it-link-attributes'
+
+const markdownWrapperClasses = 'prose prose-sm m-auto text-left'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+      reactivityTransform: true,
+    }),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue/macros',
+        '@vueuse/head',
+        '@vueuse/core',
+        'pinia',
+      ],
+      dts: 'src/autoImports.d.ts',
+      dirs: [
+        'src/server/api',
+        'src/composables',
+        'src/stores',
+      ],
+      vueTemplate: true,
+    }),
+
+    // your plugin installation
+    Components({
+      resolvers: [
+        ArcoResolver(),
+      ],
+      dts: 'src/components.d.ts',
+    }),
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss({
+      shortcuts: {
+        'border-base': 'border-gray/20 dark:border-gray/15',
+        'bg-base': 'bg-gray:15 dark:bg-[#17171A]',
+        'bg-card': 'bg-white dark:bg-[#2A2A2B]',
+        'bg-header': 'bg-white dark:bg-[#232324]',
+        'bg-sider': 'bg-white dark:bg-[#232324]',
+        'icon-btn': 'op30 hover:op100 hover:bg-[rgb(var(--primary-6))]',
+      },
+      presets: [
+        presetAttributify(),
+        presetUno(),
+        presetIcons({
+          scale: 1.2,
+        }),
+        presetWind(),
+      ],
+    }),
+
+    // https://github.com/antfu/vite-plugin-md
+    Markdown({
+      wrapperClasses: markdownWrapperClasses,
+      headEnabled: true,
+      markdownItSetup(md) {
+        // https://prismjs.com/
+        md.use(Prism)
+        md.use(LinkAttributes, {
+          matcher: (link: string) => /^https?:\/\//.test(link),
+          attrs: {
+            target: '_blank',
+            rel: 'noopener',
+          },
+        })
+      },
+    }),
+  ],
+
 })
