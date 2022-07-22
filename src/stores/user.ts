03e2513e529566855b1587e78c18b26ef25042ec
faitsse <2138889191@qq.com>
2022-07-22 09:35:54 +0800

    chore: store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,12 +1,36 @@
+import type { User } from '~/types'
+import defaultAvatar from '~/assets/default-avatar.jpg'
+
 export const userStore = defineStore(
   'userStore',
   () => {
+    const user = ref<User>()
+    /**
+     * update the new user.
+     *
+     * @param _user - new user
+     */
+    function updateUser(_user: User) {
+      if (!_user.avatar)
+        _user.avatar = defaultAvatar
+      user.value = _user
+    }
+    /**
+     * remove the user.
+     */
+    function removeUser() {
+      user.value = undefined
+    }
 
+    return {
+      user,
+      updateUser,
+      removeUser,
+    }
   },
   {
     persist: {
       enabled: true,
     },
   },
-
 )
