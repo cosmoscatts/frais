b37c53da2c5e80925db9be03836940f8624a68be
faitsse <2138889191@qq.com>
2022-07-22 09:35:21 +0800

    chore: typings

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
 import type { App } from 'vue'
 export type UserModule = (app: App) => void
 export type { App as AppContext }
+
+export * from '~/typings/user'
+export * from '~/typings/role'
+export * from '~/typings/menu'
---
+++ src/typings/user.ts
@@ -0,0 +1,48 @@
+import type { Role } from './role'
+
+export interface User {
+  /**
+   * id
+   */
+  id?: number
+  /**
+   * username
+   */
+  username?: string
+  /**
+   * name
+   */
+  name?: string
+  /**
+   * password
+   */
+  password?: string
+  /**
+   * avatar
+   */
+  avatar?: string
+  /**
+   * phone
+   */
+  phone?: string
+  /**
+   * email
+   */
+  email?: string
+  /**
+   * roleId
+   */
+  roleId?: number
+  /**
+   * role
+   */
+  role?: Role
+  /**
+   * createTime
+   */
+  createTime?: Date
+  /**
+   * updateTime
+   */
+  updateTime?: Date
+}
