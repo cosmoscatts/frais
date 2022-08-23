ae98293d2e0def94fc428de21deee9f3167c863b
faitsse <2138889191@qq.com>
2022-08-23 13:46:15 +0800

    chore: add nav components

--- src/router/guards/setupLoginGuard.ts
+++ src/router/guards/setupLoginGuard.ts
@@ -3,32 +3,6 @@ import NProgress from 'nprogress'
 
 export default function setupLoginGuard(router: Router) {
   router.beforeEach(async (to, from, next) => {
-    if (!to.path.startsWith('/redirect'))
-      NProgress.start()
-    const { user } = useUserStore()
-    if (user) {
-      if (user.roleId) {
-        next()
-      }
-      else {
-        useLogout()
-        Message.error('请联系管理员配置用户角色')
-        next('/login')
-        if (from.name === 'Login') {
-          useTimeoutFn(() => {
-            NProgress.done()
-          }, 200)
-        }
-      }
-    }
-    else {
-      if (to.name === 'Login') {
-        next()
-        return
-      }
-      next('/login')
-      if (from.name === 'Login')
-        NProgress.done()
-    }
+    next()
   })
 }
