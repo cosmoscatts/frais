53cec54aea6482296f63f87dad8ba46de7aa28ed
faitsse <2138889191@qq.com>
2022-08-23 13:13:28 +0800

    chore: update

--- src/config/appMeta.ts
+++ src/config/appMeta.ts
@@ -1,5 +1,6 @@
 export const appMeta = {
   name: 'Petite',
+  title: 'Petite Vue Template',
   description: '漂亮清新的 Vue3 后台模板',
   author: 'duende',
   github: 'https://github.com/dud9/petite',
---
+++ src/enum/request.ts
@@ -0,0 +1,4 @@
+export enum requestErrorMsgEnum {
+  notFound = '',
+  errorPage = '',
+}
