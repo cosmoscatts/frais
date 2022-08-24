cbf3c528bc5e35c7ac4773e571dd1ac2aa74db6d
faitsse <2138889191@qq.com>
2022-08-24 09:58:54 +0800

    chore: update

--- src/config/appLayout.ts
+++ src/config/appLayout.ts
@@ -2,10 +2,19 @@
  * 定义一些布局参数值（单位是 px）
  */
 export const appLayoutParams = {
+  /** 导航栏高度 */
   navHeight: 50,
+  /** 多页签高度 */
   tabHeight: 40,
+  /** 侧边栏宽度 */
   sideWidth: 240,
+  /** 侧边栏折叠后的宽度 */
   sideCollapsedWidth: 64,
+  /** 底部高度 */
   footHeight: 50,
+  /** backTop 距离页面右部的高度 */
+  backTopRight: 50,
+  /** backTop 距离页面底部的高度 */
+  backTopBottom: 200,
 }
 
