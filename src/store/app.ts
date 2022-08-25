c479b8446f93e48371c612908e4592f0cfc5b10c
faitsse <2138889191@qq.com>
2022-08-25 19:24:41 +0800

    chore: update

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,6 +1,7 @@
+import type { GlobalThemeOverrides } from 'naive-ui'
 import type { ConfigSettingObject } from '~/config'
 import { cacheAppSettings, configSettings } from '~/config'
-import { changeThemePrimaryColor, initAppSettings } from '~/utils'
+import { generatePrimaryColor, initAppSettings, setNaiveUiCommonColors } from '~/utils'
 
 export const useAppStore = defineStore(
   'appStore',
@@ -19,6 +20,24 @@ export const useAppStore = defineStore(
     const stageSettings = ref<ConfigSettingObject>({
       ...configSettings,
     })
+    // 需要覆盖的主题配置
+    // 目前只覆盖主色调
+    const themeOverrides = ref<GlobalThemeOverrides>()
+
+    const changePrimaryColor = () => {
+      // 根据主题主色调配置，生成颜色
+      const primaryColorOverrides = generatePrimaryColor(baseSettings.value.themePrimaryColor)
+      themeOverrides.value = {
+        common: {
+          ...primaryColorOverrides,
+        },
+        LoadingBar: {
+          colorLoading: baseSettings.value.themePrimaryColor,
+        },
+      }
+      // 将 naive ui 颜色写入 body
+      setNaiveUiCommonColors(primaryColorOverrides)
+    }
 
     const init = () => {
       // 如果开启了缓存配置, 则从 storage 更新配置
@@ -31,10 +50,13 @@ export const useAppStore = defineStore(
         }),
       }
 
-      // 根据主题主色调配置，生成颜色
-      // changeThemePrimaryColor(baseSettings.value.themePrimaryColor)
+      changePrimaryColor()
     }
     init()
+
+    // 当亮、暗模式改变时，更新 body 中的颜色
+    // 因为 naive ui 内置的颜色亮暗模式稍有区别
+    watch(isDark, changePrimaryColor)
 
     // 初始化 app 设置项的暂存区
     const buildStageData = () => {
@@ -53,8 +75,8 @@ export const useAppStore = defineStore(
       baseSettings.value = { ...stageSettings.value } || { ...configSettings }
 
       // 如果主题主要色调发生改变，替换主色调
-      // if (originThemePrimaryColor !== baseSettings.value.themePrimaryColor)
-      //   changeThemePrimaryColor(baseSettings.value.themePrimaryColor)
+      if (originThemePrimaryColor !== baseSettings.value.themePrimaryColor)
+        changePrimaryColor()
 
       if (cacheAppSettings)
         cacheSettingsOnStorage({ ...baseSettings.value })
@@ -64,6 +86,7 @@ export const useAppStore = defineStore(
     const { bool: menuCollapsed, setTrue: setMenuCollapsed, setFalse: setMenuUnCollapsed } = useBoolean(false)
 
     return {
+      themeOverrides,
       baseSettings,
       stageSettings,
       resetStageData,
--- src/utils/changeThemeColor.ts
+++ src/utils/changeThemeColor.ts
@@ -1,10 +1,7 @@
-import { commonDark, commonLight, useThemeVars } from 'naive-ui'
+import { commonDark, commonLight } from 'naive-ui'
+import { addColorAlpha, createHoverColor, createPressedColor } from '~/utils'
 
-import type { GlobalThemeOverrides } from 'naive-ui'
-// import { cloneDeep } from 'lodash-es'
-// import { themeSetting } from '@/settings'
-// import { EnumStorageKey } from '@/enum'
-// import { addColorAlpha, getColorPalette, getLocal, getThemeColor, removeLocal, setLocal } from '@/utils'
+const ALPHA = 0.1
 
 const colorPropertyMap: { [key: string]: string } = {
   primaryColor: '--primary-color',
@@ -30,15 +27,25 @@ const colorPropertyMap: { [key: string]: string } = {
 }
 
 /**
+ * 生成主色调的其他状态颜色，包括 hover、pressed、suppl
+ */
+export function generatePrimaryColor(_primaryColor: string) {
+  return {
+    primaryColor: _primaryColor,
+    primaryColorHover: createHoverColor(_primaryColor),
+    primaryColorPressed: createPressedColor(_primaryColor),
+    primaryColorSuppl: addColorAlpha(_primaryColor, ALPHA),
+  }
+}
+
+/**
  * 将 naive ui 的通用颜色，并写入 body
  */
-function setNaiveUiCommonColors(_primaryColorMap: { [key: string]: string }) {
+export function setNaiveUiCommonColors(_primaryColorMap: { [key: string]: string }) {
   const colors: any = isDark.value
     ? commonDark
     : commonLight
 
-  // console.log(colors)
-  // console.log(_primaryColorMap)
   const mergedColors = {
     ...colors,
     ..._primaryColorMap,
@@ -48,94 +55,3 @@ function setNaiveUiCommonColors(_primaryColorMap: { [key: string]: string }) {
   })
 }
 
-export function changeThemePrimaryColor(_color?: string) {
-  const themeVars = useThemeVars().value
-  console.log('themeVars', themeVars)
-  const {
-    primaryColor,
-    primaryColorHover,
-    primaryColorPressed,
-    primaryColorSuppl,
-  } = themeVars
-  const primaryColorMap = {
-    primaryColor,
-    primaryColorHover,
-    primaryColorPressed,
-    primaryColorSuppl,
-  }
-  setNaiveUiCommonColors(primaryColorMap)
-}
-
-/** 初始化主题配置 */
-// export function initThemeSettings() {
-//   const isProd = import.meta.env.PROD
-//   // 生产环境才缓存主题配置，本地开发实时调整配置更改配置的json
-//   const storageSettings = getThemeSettings()
-//   if (isProd && storageSettings)
-//     return storageSettings
-
-//   const themeColor = getThemeColor() || themeSetting.themeColor
-//   const info = themeSetting.isCustomizeInfoColor ? themeSetting.otherColor.info : getColorPalette(themeColor, 7)
-//   const otherColor = { ...themeSetting.otherColor, info }
-//   const setting = cloneDeep({ ...themeSetting, themeColor, otherColor })
-//   return setting
-// }
-
-// type ColorType = 'primary' | 'info' | 'success' | 'warning' | 'error'
-// type ColorScene = '' | 'Suppl' | 'Hover' | 'Pressed' | 'Active'
-// type ColorKey = `${ColorType}Color${ColorScene}`
-// type ThemeColor = Partial<Record<ColorKey, string>>
-
-// interface ColorAction {
-//   scene: ColorScene
-//   handler: (color: string) => string
-// }
-
-// /** 获取主题颜色的各种场景对应的颜色 */
-// function getThemeColors(colors: [ColorType, string][]) {
-//   const colorActions: ColorAction[] = [
-//     { scene: '', handler: color => color },
-//     { scene: 'Suppl', handler: color => color },
-//     { scene: 'Hover', handler: color => getColorPalette(color, 5) },
-//     { scene: 'Pressed', handler: color => getColorPalette(color, 7) },
-//     { scene: 'Active', handler: color => addColorAlpha(color, 0.1) },
-//   ]
-
-//   const themeColor: ThemeColor = {}
-
-//   colors.forEach((color) => {
-//     colorActions.forEach((action) => {
-//       const [colorType, colorValue] = color
-//       const colorKey: ColorKey = `${colorType}Color${action.scene}`
-//       themeColor[colorKey] = action.handler(colorValue)
-//     })
-//   })
-
-//   return themeColor
-// }
-
-// /** 获取naive的主题颜色 */
-// export function getNaiveThemeOverrides(colors: Record<ColorType, string>): GlobalThemeOverrides {
-//   const { primary, success, warning, error } = colors
-
-//   const info = themeSetting.isCustomizeInfoColor ? colors.info : getColorPalette(primary, 7)
-
-//   const themeColors = getThemeColors([
-//     ['primary', primary],
-//     ['info', info],
-//     ['success', success],
-//     ['warning', warning],
-//     ['error', error],
-//   ])
-
-//   const colorLoading = primary
-
-//   return {
-//     common: {
-//       ...themeColors,
-//     },
-//     LoadingBar: {
-//       colorLoading,
-//     },
-//   }
-// }
