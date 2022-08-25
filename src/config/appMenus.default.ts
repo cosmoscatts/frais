3f01c840389ed6b67c3e7b17cdb59729b423e2ba
faitsse <2138889191@qq.com>
2022-08-25 20:31:16 +0800

    chore: menus

--- src/config/appMenus.default.ts
+++ src/config/appMenus.default.ts
@@ -4,5 +4,36 @@ import type { Menu } from '~/types'
  * 默认菜单集合
  */
 export const defaultMenus: Menu[] = [
-
+  {
+    id: 101,
+    key: '',
+    label: '',
+    icon: '',
+    path: '',
+    children: [],
+  },
+  {
+    id: 102,
+    key: '',
+    label: '',
+    icon: '',
+    path: '',
+    children: [],
+  },
+  {
+    id: 103,
+    key: '',
+    label: '',
+    icon: '',
+    path: '',
+    children: [],
+  },
+  {
+    id: 104,
+    key: '',
+    label: '',
+    icon: '',
+    path: '',
+    children: [],
+  },
 ]
