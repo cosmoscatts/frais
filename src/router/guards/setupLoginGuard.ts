c8ca0779a3583156d87244e5c90dd8028fb76845
faitsse <2138889191@qq.com>
2022-08-26 14:36:16 +0800

    chore: 添加 loading-bar 简单示例

--- src/router/guards/setupLoginGuard.ts
+++ src/router/guards/setupLoginGuard.ts
@@ -2,6 +2,12 @@ import type { Router } from 'vue-router'
 
 export default function setupLoginGuard(router: Router) {
   router.beforeEach(async (to, from, next) => {
+    // `Loading Bar`
+    const { loadingBar } = useGlobalNaiveApi()
+    loadingBar.start()
+    useTimeoutFn(() => loadingBar.finish(), 1000)
+
+    // 获取菜单
     const permissionStore = usePermissionStore()
     permissionStore.fetchAppMenus()
     next()
