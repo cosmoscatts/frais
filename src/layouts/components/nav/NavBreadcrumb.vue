40b448d9fb8dd75af54df6986dc24bfe5ead37aa
faitsse <2138889191@qq.com>
2022-08-26 14:25:21 +0800

    chore: update

--- src/layouts/components/nav/NavBreadcrumb.vue
+++ src/layouts/components/nav/NavBreadcrumb.vue
@@ -1,19 +1,34 @@
 <script setup lang="ts">
 import { MdCash } from '@vicons/ionicons4'
 import type { RouteLocationMatched } from 'vue-router'
-const route = useRoute()
-interface Breadcrumb {}
+
+// 定义 `breadcrumb` 数据结构
+interface BreadcrumbItem {
+  title: string
+  path?: string
+}
+
 // `breadcrumb` 数据
-let metadata = $ref<string[]>()
+let metadata = $ref<BreadcrumbItem[]>()
+
+const route = useRoute()
 // 从路由获取面包屑数据
 function getBreadCrumbs() {
   metadata = []
   const matched = route.matched.filter((i: RouteLocationMatched) => i.meta && i.meta.title)
   if (!matched.length)
     return
-  metadata = matched.map(i => i.meta.title as string)
+  metadata = matched.map((i) => {
+    const { meta: { title }, path, children } = i
+    return {
+      title,
+      path: children ? undefined : path,
+    } as BreadcrumbItem
+  })
 }
 getBreadCrumbs()
+
+// 当路由发生改变时，重新生成面包屑
 watch(
   () => route.path,
   (path) => {
@@ -26,14 +41,8 @@ watch(
 
 <template>
   <n-breadcrumb separator=">">
-    <n-breadcrumb-item>
-      <n-icon><MdCash /></n-icon> 北京总行
-    </n-breadcrumb-item>
-    <n-breadcrumb-item>
-      <n-icon><MdCash /></n-icon> 天津分行
-    </n-breadcrumb-item>
-    <n-breadcrumb-item>
-      <n-icon><MdCash /></n-icon> 平山道支行
+    <n-breadcrumb-item v-for="{ title, path }, idx in metadata" :key="idx" :href="path">
+      <n-icon><MdCash /></n-icon> {{ title }}
     </n-breadcrumb-item>
   </n-breadcrumb>
 </template>
