34cceb48ae442797de6ea0f15c20848ac9b7da27
faitsse <2138889191@qq.com>
2022-08-27 14:42:22 +0800

    chore: update

--- src/layouts/components/settings/helper.ts
+++ src/layouts/components/settings/helper.ts
@@ -1,7 +1,7 @@
 /**
  * 配置项组件渲染类型
  */
-export type SettingItemRenderType = 'layoutRadio' | 'select' | 'switch'
+export type SettingItemRenderType = 'layoutRadio' | 'select' | 'switch' | 'colorPicker'
 
 /**
  * 配置项渲染结构
@@ -72,13 +72,71 @@ export const layoutSettings: SettingItem[] = [
 /**
  * 主色调配置项
  */
-export const primaryColorOption = {
-
+export const primaryColorOption: SettingItem = {
+  prop: 'themePrimaryColor',
+  name: '',
+  type: 'colorPicker',
+  options: [],
+  dependOn: undefined,
 }
 
 /**
  * 页面功能配置项
  */
-export const funcOptions = [
-
+export const funcOptions: SettingItem[] = [
+  {
+    prop: 'tabShapeStyle',
+    name: '多页签风格',
+    type: 'select',
+    options: [
+      { label: '谷歌风格', value: 'chrome' },
+      { label: '按钮风格', value: 'button' },
+    ],
+    dependOn: 'showTabs',
+  },
+  {
+    prop: 'cacheTabs',
+    name: '是否缓存多页签',
+    type: 'switch',
+    options: undefined,
+    dependOn: 'showTabs',
+  },
+  {
+    prop: 'invertMenu',
+    name: '菜单反转色',
+    type: 'switch',
+    options: undefined,
+    dependOn: undefined,
+  },
+  {
+    prop: 'sideCollapsedTriggerStyle',
+    name: '侧边栏折叠触发器样式',
+    type: 'select',
+    options: [
+      { label: '触发条', value: 'bar' },
+      { label: '触发按钮', value: 'arrow-circle' },
+    ],
+    dependOn: 'layout',
+  },
+  {
+    prop: 'showTransitionAnimation',
+    name: '是否显示页面切换动画',
+    type: 'switch',
+    options: undefined,
+    dependOn: undefined,
+  },
+  {
+    prop: 'transitionAnimation',
+    name: '页面切换动画类型',
+    type: 'select',
+    options: [
+      { label: '滑动', value: 'fade-slide' },
+      { label: '消退', value: 'fade' },
+      { label: '底部消退', value: 'fade-bottom' },
+      { label: '缩放消退', value: 'fade-scale' },
+      { label: '渐变', value: 'zoom-fade' },
+      { label: '闪现', value: 'zoom-out' },
+    ],
+    dependOn: undefined,
+  },
 ]
