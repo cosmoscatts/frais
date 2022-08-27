c9612f45680013e50dc46b7eed09234f3440cf31
faitsse <2138889191@qq.com>
2022-08-27 19:21:36 +0800

    chore: update

---
+++ src/config/appThemeColor.default.ts
@@ -0,0 +1,50 @@
+import colorJson from './color.traditional.json'
+
+/**
+ * 常用颜色
+ */
+export const themeColorList = [
+  '#1890ff',
+  '#409EFF',
+  '#2d8cf0',
+  '#007AFF',
+  '#5ac8fa',
+  '#5856D6',
+  '#536dfe',
+  '#9c27b0',
+  '#AF52DE',
+  '#0096c7',
+  '#00C1D4',
+  '#34C759',
+  '#43a047',
+  '#7cb342',
+  '#c0ca33',
+  '#78DEC7',
+  '#e53935',
+  '#d81b60',
+  '#f4511e',
+  '#fb8c00',
+  '#ffb300',
+  '#fdd835',
+  '#6d4c41',
+  '#546e7a',
+]
+
+export interface TraditionColorDetail {
+  label: string
+  color: string
+}
+
+export interface TraditionColor {
+  label: string
+  data: TraditionColorDetail[]
+}
+
+/**
+ * 中国传统颜色
+ */
+export const traditionalColorList = colorJson as TraditionColor[]
+
+export function isInTraditionalColorList(color: string) {
+  return traditionalColorList.some(i => i.data.some(v => v.color === color))
+}
--- src/layouts/components/settings/components/SettingsDrawerColorPicker.vue
+++ src/layouts/components/settings/components/SettingsDrawerColorPicker.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 import ColorCheckbox from './SettingsDrawerColorCheckBox.vue'
+import { themeColorList, traditionalColorList } from '~/config'
 
 const {
   modelValue,
-  options = [],
 } = defineProps<{
   modelValue: string
-  options: {
-    label: string
-    value: string
-  }[]
 }>()
 
 const emits = defineEmits(['update:model-value'])
@@ -17,40 +13,49 @@ const emits = defineEmits(['update:model-value'])
 function onUpdateValue(value: string) {
   emits('update:model-value', value)
 }
+
+// 是否显示传统颜色
+const showTraditionalColors = $ref(false)
 </script>
 
 <template>
   <div>
     <n-grid :cols="8" :x-gap="8" :y-gap="12">
-      <n-grid-item v-for="color in theme.themeColorList" :key="color" class="flex-x-center">
-        <ColorCheckbox :color="color" :checked="color === theme.themeColor" @click="theme.setThemeColor(color)" />
+      <n-grid-item v-for="color in themeColorList" :key="color" flex-x-center>
+        <ColorCheckbox :color="color" :checked="color === modelValue" @click="onUpdateValue(color)" />
       </n-grid-item>
     </n-grid>
-    <n-space vertical class="pt-12px">
+    <n-space vertical pt-12px>
       <n-color-picker :value="modelValue" show-alpha @update-value="onUpdateValue" />
-      <n-button block :type="otherColorBtnType" @click="openModal">
+      <n-button block strong ghost type="primary" @click="showTraditionalColors = true">
         更多颜色
       </n-button>
     </n-space>
 
-    <n-modal :show="visible" preset="card" class="w-640px h-480px" :z-index="10001" @close="handleClose">
+    <n-modal
+      :show="showTraditionalColors" preset="card"
+      style="width: 640px; height: 480px;" :z-index="10001"
+      @esc="showTraditionalColors = false"
+      @close="showTraditionalColors = false"
+      @mask-click="showTraditionalColors = false"
+    >
       <div class="flex-x-center">
         <n-gradient-text type="primary" :size="24">
           中国传统颜色
         </n-gradient-text>
       </div>
       <n-tabs>
-        <n-tab-pane v-for="item in traditionColors" :key="item.label" :name="item.label" :tab="item.label">
+        <n-tab-pane v-for="item in traditionalColorList" :key="item.label" :name="item.label" :tab="item.label">
           <n-grid :cols="8" :x-gap="16" :y-gap="8">
             <n-grid-item v-for="i in item.data" :key="i.label">
               <ColorCheckbox
                 class="!w-full !h-36px !rounded-4px"
                 :color="i.color"
-                :checked="i.color === theme.themeColor"
+                :checked="i.color === modelValue"
                 icon-class="text-20px"
-                @click="theme.setThemeColor(i.color)"
+                @click="onUpdateValue(i.color)"
               />
-              <p class="text-center">
+              <p text-center>
                 {{ i.label }}
               </p>
             </n-grid-item>
