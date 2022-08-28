f9ca4c3fd64e5ef829e704ee9c53c49e510ca2ee
faitsse <2138889191@qq.com>
2022-08-28 15:10:11 +0800

    chore: update

--- src/typings/tab.ts
+++ src/typings/tab.ts
@@ -6,8 +6,12 @@ export interface Tab {
   id: number
   /** 多页签名称 */
   title: string
+  /** 路由名称 */
+  name: string
   /** 图标 */
   icon?: string
   /** 路由 */
   path: string
+  /** 是否需要缓存页面 */
+  cached?: boolean
 }
