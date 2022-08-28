fcbc42963d1160d37f9244cf0e0b8c8c86ba7059
faitsse <2138889191@qq.com>
2022-08-28 18:57:05 +0800

    chore: update

--- src/pages/exception/error/index.vue
+++ src/pages/exception/error/index.vue
@@ -3,5 +3,13 @@
 </script>
 
 <template>
-  1
+  <n-result
+    status="500"
+    title="500 服务器错误"
+    description="服务器出错可能说明该雇更多程序员了"
+  >
+    <template #footer>
+      <n-button>散财消灾</n-button>
+    </template>
+  </n-result>
 </template>
--- src/pages/exception/not-found/index.vue
+++ src/pages/exception/not-found/index.vue
@@ -3,5 +3,9 @@
 </script>
 
 <template>
-  1
+  <n-result status="404" title="404 资源不存在" description="生活总归带点荒谬">
+    <template #footer>
+      <n-button>找点乐子吧</n-button>
+    </template>
+  </n-result>
 </template>
