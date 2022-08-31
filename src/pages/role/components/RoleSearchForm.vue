21cf4ac0116bfd935174b3ced9c8ae70fb338425
faitsse <2138889191@qq.com>
2022-08-31 15:44:18 +0800

    chore: update

--- src/pages/role/components/RoleSearchForm.vue
+++ src/pages/role/components/RoleSearchForm.vue
@@ -31,42 +31,46 @@ const searchModel = ref<SearchModel>({ ...baseSearchModel })
   <n-collapse-transition :show="showSearchForm" ha>
     <n-grid :x-gap="12" :y-gap="8" :cols="24">
       <n-gi :span="20">
-        <n-grid :x-gap="12" :y-gap="8" cols="1 s:2 l:3" responsive="screen">
+        <n-grid :x-gap="12" :y-gap="12" cols="1 s:2 l:3" responsive="screen">
           <n-gi :span="1">
-            <n-form-item label="角色名称" label-placement="left">
+            <n-form-item label="角色名称" label-placement="left" :show-feedback="false">
               <n-input v-model:value="searchModel.name" />
             </n-form-item>
           </n-gi>
           <n-gi :span="1">
-            <n-form-item label="创建时间" label-placement="left">
+            <n-form-item label="创建时间" label-placement="left" :show-feedback="false">
               <n-input v-model:value="searchModel.createTime" />
             </n-form-item>
           </n-gi>
           <n-gi :span="1">
-            <n-form-item label="更新时间" label-placement="left">
+            <n-form-item label="更新时间" label-placement="left" :show-feedback="false">
               <n-input v-model:value="searchModel.updateTime" />
             </n-form-item>
           </n-gi>
         </n-grid>
       </n-gi>
-      <n-divider vertical h-full />
+
       <n-gi :span="4">
-        <n-grid :cols="2" :x-gap="12">
-          <n-gi :span="1">
-            <n-button type="primary">
-              <template #icon>
-                <n-icon :component="SearchIcon" color="white" />
-              </template>
-              <span text-white font-bold>查询</span>
-            </n-button>
+        <n-grid :x-gap="12" :y-gap="12" cols="1 l:2" responsive="screen">
+          <n-gi span="0 s:1">
+            <div flex-y-center justify-end>
+              <n-button type="primary">
+                <template #icon>
+                  <n-icon :component="SearchIcon" color="white" />
+                </template>
+                <span text-white font-bold>查询</span>
+              </n-button>
+            </div>
           </n-gi>
-          <n-gi :span="1">
-            <n-button secondary :strong="true">
-              <template #icon>
-                <n-icon :component="RefreshIcon" />
-              </template>
-              <span font-bold>重置</span>
-            </n-button>
+          <n-gi span="0 s:1">
+            <div flex-y-center justify-end>
+              <n-button secondary :strong="true">
+                <template #icon>
+                  <n-icon :component="RefreshIcon" />
+                </template>
+                <span font-bold>重置</span>
+              </n-button>
+            </div>
           </n-gi>
         </n-grid>
       </n-gi>
