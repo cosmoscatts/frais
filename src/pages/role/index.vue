657d8e3c8edfec629b57c5526769673cb5c1a93b
faitsse <2138889191@qq.com>
2022-08-31 11:31:04 +0800

    chore: update

--- src/pages/role/index.vue
+++ src/pages/role/index.vue
@@ -4,12 +4,17 @@ import { createTableColumns, createTableData } from './helper.table'
 const { message } = useGlobalNaiveApi()
 const pagination = usePagination({})
 
+/**
+ * 创建表格序号
+ */
 function createRowNumber(rowIndex: number) {
   const { page, pageSize } = pagination
   return (page - 1) * pageSize + rowIndex + 1
 }
 
+// 创建表格数据
 const data = createTableData()
+// 创建表格列信息
 const columns = createTableColumns({
   createRowNumber,
 })
