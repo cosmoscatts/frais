4951421e08209e6634a4f8fdd7bf1248e19355d1
faitsse <2138889191@qq.com>
2022-09-01 21:30:09 +0800

    chore: update

--- src/pages/user/helper.form.ts
+++ src/pages/user/helper.form.ts
@@ -1,38 +1,56 @@
-import type { FormRules } from 'naive-ui'
+import type { FormItemRule, FormRules } from 'naive-ui'
 import type { User } from '~/types'
 
-// 表单校验规则
-export const rules: FormRules = {
-  username: [
-    {
-      required: true,
-      message: '请输入用户账号',
-    },
-  ],
-  name: [
-    {
-      required: true,
-      message: '请输入用户名称',
-    },
-  ],
-  password: [
-    {
-      required: true,
-      message: '请输入用户密码',
-    },
-  ],
-  checkPass: [
-    {
-      required: true,
-      message: '请输入确认密码',
-    },
-  ],
-  roleId: [
-    {
-      required: true,
-      message: '请选择用户角色',
-    },
-  ],
+/**
+ * 生成表单校验规则
+ */
+export function createRules(
+  validatePasswordStartWith: (rule: FormItemRule, value: string) => boolean,
+  validatePasswordSame: (rule: FormItemRule, value: string) => boolean,
+): FormRules {
+  return {
+    username: [
+      {
+        required: true,
+        message: '请输入用户账号',
+      },
+    ],
+    name: [
+      {
+        required: true,
+        message: '请输入用户名称',
+      },
+    ],
+    password: [
+      {
+        required: true,
+        message: '请输入用户密码',
+      },
+    ],
+    reenteredPassword: [
+      {
+        required: true,
+        message: '请再次输入密码',
+        trigger: ['input', 'blur'],
+      },
+      {
+        validator: validatePasswordStartWith,
+        message: '两次密码输入不一致',
+        trigger: 'input',
+      },
+      {
+        validator: validatePasswordSame,
+        message: '两次密码输入不一致',
+        trigger: ['blur', 'password-input'],
+      },
+    ],
+    roleId: [
+      {
+        required: true,
+        message: '请选择用户角色',
+      },
+    ],
+  }
 }
 
 /** `form-action` 类型  */
