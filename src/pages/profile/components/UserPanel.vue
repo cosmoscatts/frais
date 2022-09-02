17bb9ecd347db29076882168e127c9a6db67c029
faitsse <2138889191@qq.com>
2022-09-02 11:03:01 +0800

    chore: update

--- src/pages/profile/components/UserPanel.vue
+++ src/pages/profile/components/UserPanel.vue
@@ -1,7 +1,154 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+const userStore = useUserStore()
+const { user } = storeToRefs(userStore)
 
+/**
+ * 将角色 `id` 转换为名称
+ */
+function transformRoleName(_roleId?: number) {
+  return '管理员'
+}
+
+/**
+ * 格式化时间
+ */
+function formatDate(date?: Date) {
+  return dayJs(date).format('YYYY-MM-DD HH:mm:ss') || '-'
+}
+
+function getFileUrl() {
+  const avatar = unref(user)?.avatar
+  return avatar
+    ? {
+        url: avatar,
+      }
+    : undefined
+}
+
+const file = ref<any>(getFileUrl())
+const avatar = computed(() => {
+  return unref(user)?.avatar
+})
+watch(avatar, () => {
+  file.value = getFileUrl()
+})
+
+function onChange(_: FileItem[], currentFile: FileItem) {
+  file.value = {
+    ...currentFile,
+  }
+  getBase64(unref(file).file).then(async (imageAsDateURL) => {
+    // const formData = {
+    //   id: unref(user)?.id,
+    //   avatar: imageAsDateURL,
+    // }
+    // const { code } = await UserApi.updateAvatar(formData) as any
+    // if (code === 0) {
+    Message.success('上传成功')
+    userStore.updateAvatar(imageAsDateURL as string)
+    // }
+    // else {
+    //   Message.error('上传失败')
+    // }
+  })
+}
+
+function getBase64(file: any) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    let imageAsDateURL = ''
+    reader.readAsDataURL(file)
+    reader.onload = (data) => {
+      const res: any = data.target || data.srcElement
+      imageAsDateURL = res.result
+    }
+    reader.onerror = (err) => {
+      reject(err)
+    }
+    reader.onloadend = () => {
+      resolve(imageAsDateURL)
+    }
+  })
+}
+
+const { width } = useWindowSize()
+const imagePreviewVisible = ref(false)
+
+function beforeUpload(file: any): any {
+  return new Promise((resolve) => {
+    if (!file.type.startsWith('image')) {
+      Message.error('请上传图片')
+      return resolve(false)
+    }
+    resolve(true)
+  })
+}
 </script>
 
 <template>
-  1
+  <n-card>
+    <n-descriptions label-placement="left">
+      <n-descriptions-item>
+        <template #label>
+          <n-h5 prefix="bar" align-text style="margin-bottom: 0;" flex-inline>
+            账号
+          </n-h5>
+        </template>
+        <n-tag type="warning">
+          {{ user?.username ?? '-' }}
+        </n-tag>
+      </n-descriptions-item>
+      <n-descriptions-item>
+        <template #label>
+          <n-h5 prefix="bar" align-text style="margin-bottom: 0;" flex-inline>
+            名称
+          </n-h5>
+        </template>
+        <n-tag>
+          {{ user?.name ?? '-' }}
+        </n-tag>
+      </n-descriptions-item>
+      <n-descriptions-item>
+        <template #label>
+          <n-h5 prefix="bar" align-text style="margin-bottom: 0;" flex-inline>
+            手机号码
+          </n-h5>
+        </template>
+        <n-tag>
+          {{ user?.phone ?? '无' }}
+        </n-tag>
+      </n-descriptions-item>
+      <n-descriptions-item>
+        <template #label>
+          <n-h5 prefix="bar" align-text style="margin-bottom: 0;" flex-inline>
+            邮箱
+          </n-h5>
+        </template>
+        <n-tag>
+          {{ user?.email ?? '无' }}
+        </n-tag>
+      </n-descriptions-item>
+      <n-descriptions-item>
+        <template #label>
+          <n-h5 prefix="bar" align-text style="margin-bottom: 0;" flex-inline>
+            角色
+          </n-h5>
+        </template>
+        <n-tag type="info">
+          {{ transformRoleName(user?.roleId) }}
+        </n-tag>
+      </n-descriptions-item>
+      <n-descriptions-item>
+        <template #label>
+          <n-h5 prefix="bar" align-text style="margin-bottom: 0;" flex-inline>
+            创建时间
+          </n-h5>
+        </template>
+        <n-tag>
+          {{ formatDate(user?.createTime) ?? '-' }}
+        </n-tag>
+      </n-descriptions-item>
+    </n-descriptions>
+  </n-card>
 </template>
+
--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -1,5 +1,25 @@
+<script setup lang="ts">
+import UserPanel from './components/UserPanel.vue'
+import BasicInformation from './components/BasicInformation.vue'
+import SecuritySettings from './components/SecuritySettings.vue'
+
+// 当前显示的 `tab` 页
+const currentTab = ref(1)
+</script>
+
 <template>
-  <div text-xl font-bold>
-    Profile
+  <div flex="~ col">
+    <UserPanel />
+
+    <n-card mt-20px>
+      <n-tabs v-model:value="currentTab" type="card" animated w-full>
+        <n-tab-pane :name="1" tab="信息修改">
+          <BasicInformation />
+        </n-tab-pane>
+        <n-tab-pane :name="2" tab="安全设置">
+          <SecuritySettings />
+        </n-tab-pane>
+      </n-tabs>
+    </n-card>
   </div>
 </template>
