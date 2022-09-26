b8060c756d463b3d74dab48120788f165e97347c
faitsse <2138889191@qq.com>
2022-09-26 20:28:25 +0800

    chore: update

--- src/config/appMeta.ts
+++ src/config/appMeta.ts
@@ -5,7 +5,7 @@ export const appMeta = {
   name: 'Petite',
   title: 'Petite管理系统',
   description: '漂亮清新的 Vue3 后台模板',
-  author: 'duende',
+  author: 'Cosmoscatts',
   github: 'https://github.com/cosmoscatts/petite',
   doc: 'https://github.com/cosmoscatts/petite',
   footCopyRight: 'Petite 1.0 - Made by Cosmoscatts',
